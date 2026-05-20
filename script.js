1fac99a7999a29921f27b82c6d533a665c2515e3
DitebogoSolomon <bida25-204@thuto.bac.ac.bw>
2026-05-20 02:16:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.getElementById("myForm").addEventListener("submit", function(e) {
+    e.preventDefault(); // stop form from submitting
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let message = document.getElementById("message").value.trim();
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill in all fields");
+        return;
+    }
+
+    if (!email.includes("@")) {
+        alert("Enter a valid email");
+        return;
+    }
+
+    alert("Form submitted successfully!");
+
+    // Optional: clear form
+    this.reset();
+});
+
+
+
+
+
+
+
+
+
+
+
+
